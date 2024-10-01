e2fdbba45a8a44eeb753853cbf7305d42491727b
jmsjrz <panzersgrphone@gmail.com>
2024-10-01 04:11:11 +0200

    chore: Add Django project files

---
+++ planr_backend/planr_backend/urls.py
@@ -0,0 +1,8 @@
+# planr_backend/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
+]
