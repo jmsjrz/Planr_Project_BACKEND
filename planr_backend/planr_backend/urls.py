bfebbfe5a2c62a1fa2312c1b4af959389193c8bc
jmsjrz <panzersgrphone@gmail.com>
2024-10-06 13:10:19 +0200

    feat: Update URLs configuration to use Django REST Framework routers

--- planr_backend/planr_backend/urls.py
+++ planr_backend/planr_backend/urls.py
@@ -1,8 +1,14 @@
-# planr_backend/urls.py
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from authentication.views import UserViewSet
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls')),
 ]
