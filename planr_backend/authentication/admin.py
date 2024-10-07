db17f470c96c28222d7f416052a27f43447b90ee
jmsjrz <panzersgrphone@gmail.com>
2024-10-07 16:59:53 +0200

    chore: Update PasswordResetAttemptAdmin to make requested_at, ip_address, and user_agent fields readonly

--- planr_backend/authentication/admin.py
+++ planr_backend/authentication/admin.py
@@ -6,3 +6,4 @@ class PasswordResetAttemptAdmin(admin.ModelAdmin):
     list_display = ('user', 'requested_at', 'ip_address', 'user_agent')
     search_fields = ('user__email', 'user__phone_number', 'ip_address')
     list_filter = ('requested_at',)
+    readonly_fields = ('requested_at', 'ip_address', 'user_agent')
