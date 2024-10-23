11b5d4fdb9c6d006cfad8d53efb4c72c894bfd18
jmsjrz <panzersgrphone@gmail.com>
2024-10-23 07:23:04 +0200

    feat: Update admin.py for events app

--- planr_backend/events/admin.py
+++ planr_backend/events/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import PrivateEvent, ProfessionalEvent, Service, EventRegistration, Wishlist
 
-# Register your models here.
+@admin.register(PrivateEvent)
+class PrivateEventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'date', 'time', 'max_participants')
+    search_fields = ('title', 'location')
+    list_filter = ('date', 'location')
+    ordering = ('date', 'time')
+
+@admin.register(ProfessionalEvent)
+class ProfessionalEventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'date', 'time', 'price', 'max_participants')
+    search_fields = ('title', 'location', 'price')
+    list_filter = ('date', 'location', 'price')
+    ordering = ('date', 'time')
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(EventRegistration)
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'registered_at', 'payment_status')
+    search_fields = ('user__email', 'event__title')
+    list_filter = ('payment_status', 'registered_at')
+    ordering = ('-registered_at',)
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event')
+    search_fields = ('user__email', 'event__title')
+    ordering = ('user',)
+
